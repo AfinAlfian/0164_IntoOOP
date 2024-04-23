5171e55148e63bb7e324ce955f53d8ccfb144d11
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-04-23 11:35:33 +0700

    menambahkan data prosedur input

--- oop2/oop2.cpp
+++ oop2/oop2.cpp
@@ -6,6 +6,12 @@ class bangunDatar {
         float panjang, lebar;
     public:
         float luas;
+        void input() {
+            cout << "Masukkan Panjangnya = ";
+            cin >> panjang;
+            cout << "Masukkan Lebarnya = ";
+            cin >> lebar;
+        }
 };
 
 int main()
